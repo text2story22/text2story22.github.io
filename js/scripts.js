e974ed4923252e8f1c35a58f01be589c6e2bad52
hugo.o.sousa <hugo.o.sousa@inesctec.pt>
2021-10-26 11:47:56 +0100

    code clean up

--- js/scripts.js
+++ js/scripts.js
@@ -51,42 +51,4 @@ jQuery(function ($) {
         });
     }());
 
-    // --------------------------------------------------------------------
-    // Map
-    // --------------------------------------------------------------------
-
-    // (function () {
-    //     var latitude  = 50.944512; //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-    //     var longitude = 6.9503217;
-    //     var map_zoom  = 14;
-
-    //     var platform = new H.service.Platform({
-    //         'app_id': 'IwGH4C4l25qnAx4Ysqsb',
-    //         'app_code': 'j3LDYCZGo1TBuCP3lxwEqw'
-    //       });
-
-    //       // Obtain the default map types from the platform object:
-    //         var defaultLayers = platform.createDefaultLayers();
-
-    //         // Instantiate (and display) a map object:
-    //         var map = new H.Map(
-    //         document.getElementById('mapContainer'),
-    //         defaultLayers.normal.map);
-
-    //         // center address
-    //         map.setCenter({lat:latitude, lng:longitude});
-    //         map.setZoom(map_zoom);
-
-    //         // add marker
-    //         map.addObject(new H.map.Marker({lat:latitude, lng:longitude}));
-
-    //         //Step 3: make the map interactive
-    //         // MapEvents enables the event system
-    //         // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-    //         var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-    //         // Create the default UI components
-    //         var ui = H.ui.UI.createDefault(map, defaultLayers);
-
-    // }());
-
 }); // JQuery end
